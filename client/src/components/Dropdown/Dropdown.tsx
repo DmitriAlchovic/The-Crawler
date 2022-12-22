5fc27da7d9adeda85959051eca6e652a17e905b3
d.alkhovich <d.alkhovich@syberry.com>
2022-12-22 16:17:59 +0300

    490297: Add navbar

---
+++ client/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from 'react';
+import chevron from '../../assets/chevron.svg';
+import styles from './Dropdown.module.sass';
+
+interface DropdownProps {
+  children: React.ReactNode;
+  hasChildrenClose: boolean;
+}
+
+export default function Dropdown({
+  children,
+  hasChildrenClose,
+}: DropdownProps) {
+  const [isShown, setIsShown] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setIsShown(false);
+    });
+  }, []);
+
+  return (
+    <div className={styles['drop-container']}>
+      <div
+        className={styles['drop-btn']}
+        aria-label="label"
+        tabIndex={-1}
+        role="button"
+        onKeyDown={() => {}}
+        onClick={() => {
+          setIsShown(!isShown);
+        }}
+      >
+        <span className={isShown ? styles['chevron-drop'] : styles.chevron}>
+          <img alt="chevron" src={chevron} />
+        </span>
+      </div>
+
+      <div>
+        {isShown && (
+          <div
+            aria-label="label"
+            tabIndex={-1}
+            role="button"
+            onKeyDown={() => {}}
+            className={styles['drop-background']}
+            onClick={() => {
+              setIsShown(!isShown);
+            }}
+          />
+        )}
+        <div
+          className={isShown ? styles['children-container'] : styles.hiden}
+          aria-label="label"
+          tabIndex={-1}
+          role="button"
+          onKeyDown={() => {}}
+          onClick={() => {
+            if (hasChildrenClose) {
+              setIsShown(!isShown);
+            }
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
